3cbb4d74d997d183e3446f7215a0bf46190d4dff
dabe4v3r <apejfu@gmail.com>
2026-04-17 19:38:44 +0300

    виконання практичної 9

--- practice/9/practice9.asm
+++ practice/9/practice9.asm
@@ -1,36 +1,135 @@
-; practice9.asm
-; I/O: int 80h
-; blocks: I/O, parse, math/logic, loops, memory
+section .bss
+    inbuf   resb 16
+    freq    resd 10
+    linebuf resb 80           
 
-BITS 32
-GLOBAL _start
+section .text
+global _start
 
-SECTION .data
-prompt db "practice9: see README.md", 10
-prompt_len equ $-prompt
-
-SECTION .bss
-buf resb 256
-
-SECTION .text
 _start:
-    ; I/O: write prompt
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, prompt
-    mov edx, prompt_len
-    int 0x80
 
-    ; I/O: read line (optional in skeleton)
-    mov eax, 3          ; sys_read
-    mov ebx, 0          ; stdin
-    mov ecx, buf
-    mov edx, 255
-    int 0x80
+    mov  eax, 3
+    mov  ebx, 0
+    mov  ecx, inbuf
+    mov  edx, 16
+    int  0x80
 
-    ; logic: TODO implement task logic according to README.md
+    mov  esi, inbuf
+    xor  ebp, ebp
+.parse_loop:
+    movzx eax, byte [esi]
+    inc   esi
+    cmp   al, '0'
+    jb    .parse_done
+    cmp   al, '9'
+    ja    .parse_done
+    sub   al, '0'
+    imul  ebp, ebp, 10
+    add   ebp, eax
+    jmp   .parse_loop
+.parse_done:
 
-    ; exit
-    mov eax, 1          ; sys_exit
-    xor ebx, ebx
-    int 0x80
+    mov  edi, freq
+    xor  eax, eax
+    mov  ecx, 10
+    rep  stosd
+
+    mov  ebx, 12345
+
+.gen_loop:
+    test ebp, ebp
+    jz   .gen_done
+
+    mov  eax, 1103515245
+    imul eax, ebx
+    add  eax, 12345
+    and  eax, 0x7FFFFFFF
+    mov  ebx, eax
+
+    xor  edx, edx
+    mov  ecx, 10
+    div  ecx
+
+    inc  dword [freq + edx*4]
+
+    dec  ebp
+    jmp  .gen_loop
+
+.gen_done:
+
+    xor  ebx, ebx
+
+.print_loop:
+    cmp  ebx, 10
+    jge  .exit
+
+    mov  edi, linebuf
+
+    mov  al, bl
+    add  al, '0'
+    stosb
+    mov  al, ':'
+    stosb
+    mov  al, ' '
+    stosb
+
+    mov  eax, [freq + ebx*4]
+    push eax
+
+    mov  ecx, eax
+    test ecx, ecx
+    jz   .hashes_done
+    mov  al, '#'
+.hash_loop:
+    stosb
+    loop .hash_loop
+.hashes_done:
+
+    mov  al, ' '
+    stosb
+    mov  al, '('
+    stosb
+
+    pop  eax
+    push ebx
+
+    xor  ecx, ecx
+.to_ascii:
+    xor  edx, edx
+    push ecx
+    mov  ecx, 10
+    div  ecx
+    pop  ecx
+    push edx
+    inc  ecx
+    test eax, eax
+    jnz  .to_ascii
+
+.write_digits:
+    pop  eax
+    add  al, '0'
+    stosb
+    loop .write_digits
+
+    mov  al, ')'
+    stosb
+    mov  al, 10
+    stosb
+
+    mov  edx, edi
+    sub  edx, linebuf
+    pop  ebx
+    push ebx
+    mov  eax, 4
+    mov  ebx, 1
+    mov  ecx, linebuf
+    int  0x80
+    pop  ebx
+
+    inc  ebx
+    jmp  .print_loop
+
+.exit:
+    mov  eax, 1
+    xor  ebx, ebx
+    int  0x80
