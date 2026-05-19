13000adb6332fe2a29f518c443d7705f373aa0cc
dabe4v3r <apejfu@gmail.com>
2026-05-19 10:18:26 +0300

    виконання практичної 15

--- practice/15/practice15.asm
+++ practice/15/practice15.asm
@@ -1,36 +1,144 @@
-; practice15.asm
-; I/O: int 80h
-; blocks: I/O, parse, math/logic, loops, memory
+section .bss
+    calls   resd 1
+    result  resd 1
 
-BITS 32
-GLOBAL _start
+section .data
+    msg_fact      db "fact(n) = ", 0
+    msg_fact_len  equ $ - msg_fact
+    msg_calls     db "calls   = ", 0
+    msg_calls_len equ $ - msg_calls
+    buf           times 14 db 0
 
-SECTION .data
-prompt db "practice15: see README.md", 10
-prompt_len equ $-prompt
+section .text
+global _start
 
-SECTION .bss
-buf resb 256
+; Вхід: eax = n   Вихід: eax = n!
+fact:
+    push ebp
+    mov  ebp, esp
+    push ecx
+    push edx
 
-SECTION .text
+    inc  dword [calls]
+
+    cmp  eax, 1
+    jle  .base
+
+    mov  ecx, eax
+    dec  eax
+    call fact
+    imul eax, ecx
+    jmp  .done
+
+.base:
+    mov  eax, 1
+
+.done:
+    pop  edx
+    pop  ecx
+    pop  ebp
+    ret
+
+; Вхід: eax = число   Вихід: ecx = вказівник, edx = довжина
+uint_to_str:
+    push ebp
+    mov  ebp, esp
+    push ebx
+    push edi
+
+    lea  edi, [buf+12]
+    mov  byte [edi], 10     ; '\n'
+    dec  edi
+
+    test eax, eax
+    jnz  .convert
+    mov  byte [edi], '0'
+    dec  edi
+    jmp  .finish
+
+.convert:
+    mov  ebx, 10
+.loop:
+    test eax, eax
+    jz   .finish
+    xor  edx, edx
+    div  ebx
+    add  dl, '0'
+    mov  [edi], dl
+    dec  edi
+    jmp  .loop
+
+.finish:
+    lea  ecx, [edi+1]
+    lea  edx, [buf+13]
+    sub  edx, edi
+
+    pop  edi
+    pop  ebx
+    pop  ebp
+    ret
+
 _start:
-    ; I/O: write prompt
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, prompt
-    mov edx, prompt_len
-    int 0x80
+    mov  eax, 3
+    mov  ebx, 0
+    mov  ecx, buf
+    mov  edx, 13
+    int  0x80
 
-    ; I/O: read line (optional in skeleton)
-    mov eax, 3          ; sys_read
-    mov ebx, 0          ; stdin
-    mov ecx, buf
-    mov edx, 255
-    int 0x80
+    mov  esi, buf
+    xor  eax, eax
+    xor  ecx, ecx
 
-    ; logic: TODO implement task logic according to README.md
+.parse_loop:
+    movzx edx, byte [esi+ecx]
+    cmp  dl, '0'
+    jl   .parse_done
+    cmp  dl, '9'
+    jg   .parse_done
+    imul eax, eax, 10
+    sub  dl, '0'
+    add  eax, edx
+    inc  ecx
+    jmp  .parse_loop
 
-    ; exit
-    mov eax, 1          ; sys_exit
-    xor ebx, ebx
-    int 0x80
+.parse_done:
+    cmp  eax, 12
+    jle  .n_ok
+    mov  eax, 12
+.n_ok:
+    mov  dword [calls], 0
+
+    call fact
+    mov  [result], eax
+
+    ; друкуємо мітку
+    mov  eax, 4
+    mov  ebx, 1
+    mov  ecx, msg_fact
+    mov  edx, msg_fact_len
+    int  0x80
+
+    ; друкуємо факторіал
+    mov  eax, [result]
+    call uint_to_str
+    mov  eax, 4
+    mov  ebx, 1
+    int  0x80
+
+    ; друкуємо мітку
+    mov  eax, 4
+    mov  ebx, 1
+    mov  ecx, msg_calls
+    mov  edx, msg_calls_len
+    int  0x80
+
+    ; друкуємо calls
+    mov  eax, [calls]
+    call uint_to_str
+    mov  eax, 4
+    mov  ebx, 1
+    int  0x80
+
+    mov  eax, 1
+    xor  ebx, ebx
+    int  0x80
